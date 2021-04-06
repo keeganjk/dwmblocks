335757916d8ff45a42f1c0b7601d317141055e3e
keeganjk <24554125+keeganjk@users.noreply.github.com>
2021-04-06 14:38:53 -0400

    fixed incorrect command, removed scripts [for now?] since they are in dotfiles repo

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                /*Update Interval*/	/*Update Signal*/
-    {"",        "localIP",                      600,                601},
+    {"",        "locip",                        600,                601},
     //{"",        "volume",                       0,                  10},
     //{"",        "battery",                      300,                302},
     {"",       "checkupdates+aur | wc -l",     0,                  20},

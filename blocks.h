2ea25595281566227f28585f8210e4e6654f6d97
keeganjk <24554125+keeganjk@users.noreply.github.com>
2021-04-30 17:04:25 -0400

    changed up some stuff, apparently fixed some issues

--- blocks.h
+++ blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                /*Update Interval*/	/*Update Signal*/
-    {"",        "locip",                        600,                601},
-    //{"",        "volume",                       0,                  10},
-    //{"",        "battery",                      300,                302},
-    {"UPD:",       "checkupdates+aur | wc -l",     600,                20},
-    {"CPU:",       "cpu-percentage",               300,                301},
-	{"MEM:",       "mem-percentage",	            300,	            300},
-	{"",        "date '+[%w] %Y.%m.%d %H:%M'",  60,	            	60},
+    {"",          "locip",                          600,                11},
+    //{"",        "volume",                       0,                  16},
+    //{"",        "battery",                      300,                17},
+    {"UPD:",       "checkupdates+aur | wc -l",      600,                12},
+    {"CPU:",       "cpu-percentage",                300,                13},
+	{"MEM:",       "mem-percentage",                300,	            14},
+	{"",           "date '+[%w] %Y-%m-%d %H:%M'",   60,	            	15},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
